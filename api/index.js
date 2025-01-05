119d0d8269db9173b5453c594b1ad27559506e6f
roshan6022 <roshansingh6.022@gmail.com>
2025-01-05 20:41:54 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO) // to use this we need to import dotenv package;
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
